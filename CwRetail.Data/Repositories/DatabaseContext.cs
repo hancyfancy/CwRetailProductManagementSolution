f5e3d51a75b599470bf8911d308a3c96148bb322
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-10-03 22:22:13 +1100

    Update DatabaseContext to contain reference to product repository

--- CwRetail.Data/Repositories/DatabaseContext.cs
+++ CwRetail.Data/Repositories/DatabaseContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CwRetail.Data.Repositories.Implementation;
+using CwRetail.Data.Repositories.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,16 @@ namespace CwRetail.Data.Repositories
 {
     public class DatabaseContext
     {
+        private static readonly IProductRepository _products;
+
+        static DatabaseContext()
+        {
+            _products = new ProductRepository();
+        }
+
+        public static IProductRepository Products
+        { 
+            get { return _products; }
+        }
     }
 }
