59e18c925b302bd287acf6803746732b6551a5d1
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-10-04 23:40:25 +1100

    Add try catch to AsUpdateSql method

--- CwRetail.Data/Extensions/DatabaseExtension.cs
+++ CwRetail.Data/Extensions/DatabaseExtension.cs
@@ -11,34 +11,41 @@ namespace CwRetail.Data.Extensions
     {
         public static string AsUpdateSql(this string json)
         {
-            string output = string.Empty;
-
-            string properties = json.Replace("{", "").Replace("}", "").Replace("\r\n","").Trim();
-
-            List<string> props = properties.Split(",").ToList();
-
-            for (int i = 0; i < props.Count; i++)
+            try
             {
-                string prop = props[i];
+                string output = string.Empty;
 
-                List<string> kv = prop.Split(":").ToList();
+                string properties = json.Replace("{", "").Replace("}", "").Replace("\r\n", "").Trim();
 
-                if (i > 0)
+                List<string> props = properties.Split(",").ToList();
+
+                for (int i = 0; i < props.Count; i++)
                 {
-                    output += ",";
+                    string prop = props[i];
+
+                    List<string> kv = prop.Split(":").ToList();
+
+                    if (i > 0)
+                    {
+                        output += ",";
+                    }
+
+                    if (string.Equals(kv[0].Replace("\"", "").Trim(), "Type") && (!kv[1].Replace("\"", "").Trim().All(Char.IsLetter)))
+                    {
+                        int enumInt = int.Parse(kv[1]);
+
+                        kv[1] = "'" + ((ProductEnum)enumInt).ToString() + "'";
+                    }
+
+                    output += kv[0].Replace("\"", "") + "=" + kv[1].Replace("\"", "'").Replace("true", "1").Replace("false", "0");
                 }
 
-                if (string.Equals(kv[0].Replace("\"","").Trim(), "Type") && (!kv[1].Replace("\"", "").Trim().All(Char.IsLetter)))
-                {
-                    int enumInt = int.Parse(kv[1]);
-
-                    kv[1] = "'" + ((ProductEnum)enumInt).ToString() + "'";
-                }
-
-                output += kv[0].Replace("\"","") + "=" + kv[1].Replace("\"","'").Replace("true","1").Replace("false","0");
+                return output;
             }
-
-            return output;
+            catch (Exception e)
+            {
+                throw;
+            }
         }
     }
 }
