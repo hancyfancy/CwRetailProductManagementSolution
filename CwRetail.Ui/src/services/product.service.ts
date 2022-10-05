8ace0e9b685f08145b5d05c225ded4bfa7f0baf2
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-10-05 23:40:03 +1100

    Modify ui structure

--- CwRetail.Ui/src/services/product.service.ts
+++ CwRetail.Ui/src/services/product.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 
-import { Product } from './product';
+import { Product } from '../models/product';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
