26c73ec70fbd8e64a2661a3645b1328b28cfb896
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-10-05 20:26:28 +1100

    Add routing

--- CwRetail.Ui/src/app/app.module.ts
+++ CwRetail.Ui/src/app/app.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductComponent } from './product/product.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductComponent },
+      { path: 'details', component: ProductDetailComponent },
+      { path: '', redirectTo: '/products', pathMatch: 'full' },
+      { path: '**', component: NotFoundComponent }
+    ]),
   ],
   declarations: [
     AppComponent,
