bd7dc5f152224122b4bd7fb64eea44c1f3463943
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-10-08 21:57:20 +1100

    Create mock unit test for get

--- CwRetail.Data.Test/Repositories/ProductRepositoryTests.cs
+++ CwRetail.Data.Test/Repositories/ProductRepositoryTests.cs
@@ -1,9 +1,12 @@
-﻿using CwRetail.Data.Models;
+﻿using Autofac.Extras.Moq;
+using CwRetail.Data.Enumerations;
+using CwRetail.Data.Models;
 using CwRetail.Data.Repositories;
 using CwRetail.Data.Repositories.Implementation;
 using CwRetail.Data.Repositories.Interface;
 using CwRetail.Data.Test.Repositories.TestData;
 using Microsoft.VisualStudio.TestPlatform.PlatformAbstractions.Interfaces;
+using Moq;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -28,6 +31,50 @@ namespace CwRetail.Data.Test.Repositories
             List<Product> products = _repo.Get().ToList();
 
             Assert.IsType<List<Product>>(products);
+        }
+
+        [Fact]
+        public void GetMockTest()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IProductRepository>()
+                    .Setup(x => x.Get())
+                    .Returns(GetSampleProducts());
+
+                var repo = mock.Create<IProductRepository>();
+
+                var expected = GetSampleProducts();
+                var actual = repo.Get().ToList();
+
+                Assert.True((actual != null) && (actual?.Count > 0));
+
+                Assert.Equal(expected.Count, actual?.Count);
+
+                Assert.Equal(JsonConvert.SerializeObject(expected), JsonConvert.SerializeObject(actual));
+            }
+        }
+
+        private List<Product> GetSampleProducts()
+        {
+            return new List<Product>() { 
+                new Product()
+                { 
+                    Id = 3,
+                    Name = "34107476-E228-4AD1-9C04-46ECA69BDE92",
+                    Price = 1913.57M,
+                    Type = ProductTypeEnum.Food,
+                    Active = true,
+                },
+                new Product()
+                { 
+                    Id = 4,
+                    Name = "AE8D7304-DB95-4D25-B04F-D3D36570990A",
+                    Price = 1193.22M,
+                    Type = ProductTypeEnum.Furniture,
+                    Active = false,
+                }
+            };
         }
 
         [Theory]
