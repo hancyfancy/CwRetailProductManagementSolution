f4a44d294883418fc8765f567e5779f5fe6b5fad
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-10-08 23:07:29 +1100

    Create database extension tests, need to create more test data

---
+++ CwRetail.Data.Test/Extensions/DatabaseExtensionTests.cs
@@ -0,0 +1,33 @@
+﻿using Autofac.Extras.Moq;
+using CwRetail.Data.Extensions;
+using CwRetail.Data.Models;
+using CwRetail.Data.Repositories.Interface;
+using CwRetail.Data.Test.Extensions.TestData;
+using CwRetail.Data.Test.Repositories.TestData;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CwRetail.Data.Test.Extensions
+{
+    public class DatabaseExtensionTests
+    {
+        public DatabaseExtensionTests()
+        { 
+            
+        }
+
+        [Theory]
+        [ClassData(typeof(DatabaseExtensionAsUpdateSqlTestData))]
+        public void AsUpdateSqlTest(string expectedOutput, string input)
+        {
+            string output = input.AsUpdateSql();
+
+            Assert.Equal(expectedOutput, output);
+        }
+    }
+}
---
+++ CwRetail.Data.Test/Extensions/TestData/DatabaseExtensionAsUpdateSqlTestData.cs
@@ -0,0 +1,25 @@
+﻿using CwRetail.Data.Enumerations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CwRetail.Data.Test.Extensions.TestData
+{
+    public class DatabaseExtensionAsUpdateSqlTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { Regex.Replace("Name= 'AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA',    Price= 54.35,    Type='Books',    Active= 0", @"\s+", ""), Regex.Replace("{\r\n    \"Name\": \"AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA\",\r\n    \"Price\": 54.35,\r\n    \"Type\": 1,\r\n    \"Active\": false\r\n}", @"\s+", "") };
+            yield return new object[] { Regex.Replace("", @"\s+", ""), Regex.Replace("", @"\s+", "") };
+            yield return new object[] { Regex.Replace("", @"\s+", ""), Regex.Replace("", @"\s+", "") };
+            yield return new object[] { Regex.Replace("", @"\s+", ""), Regex.Replace("", @"\s+", "") };
+            yield return new object[] { Regex.Replace("", @"\s+", ""), Regex.Replace("", @"\s+", "") };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
