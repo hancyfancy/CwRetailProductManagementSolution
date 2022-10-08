903e0822bd4139f5de2d890d7040270d57bb75df
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-10-08 23:34:12 +1100

    Create unit test data for AsUpdateSql method

--- CwRetail.Data.Test/Extensions/TestData/DatabaseExtensionAsUpdateSqlTestData.cs
+++ CwRetail.Data.Test/Extensions/TestData/DatabaseExtensionAsUpdateSqlTestData.cs
@@ -14,10 +14,10 @@ namespace CwRetail.Data.Test.Extensions.TestData
         public IEnumerator<object[]> GetEnumerator()
         {
             yield return new object[] { Regex.Replace("Name= 'AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA',    Price= 54.35,    Type='Books',    Active= 0", @"\s+", ""), Regex.Replace("{\r\n    \"Name\": \"AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA\",\r\n    \"Price\": 54.35,\r\n    \"Type\": 1,\r\n    \"Active\": false\r\n}", @"\s+", "") };
-            yield return new object[] { Regex.Replace("", @"\s+", ""), Regex.Replace("", @"\s+", "") };
-            yield return new object[] { Regex.Replace("", @"\s+", ""), Regex.Replace("", @"\s+", "") };
-            yield return new object[] { Regex.Replace("", @"\s+", ""), Regex.Replace("", @"\s+", "") };
-            yield return new object[] { Regex.Replace("", @"\s+", ""), Regex.Replace("", @"\s+", "") };
+            yield return new object[] { Regex.Replace("Name= 'AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA'", @"\s+", ""), Regex.Replace("{\r\n    \"Name\": \"AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA\"\r\n}", @"\s+", "") };
+            yield return new object[] { Regex.Replace("Price= 54.35", @"\s+", ""), Regex.Replace("{\r\n    \"Price\": 54.35\r\n}", @"\s+", "") };
+            yield return new object[] { Regex.Replace("Type='Books'", @"\s+", ""), Regex.Replace("{\r\n    \"Type\": 1\r\n}", @"\s+", "") };
+            yield return new object[] { Regex.Replace("Active= 0", @"\s+", ""), Regex.Replace("{\r\n    \"Active\": false\r\n}", @"\s+", "") };
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
