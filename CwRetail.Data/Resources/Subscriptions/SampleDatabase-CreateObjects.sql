aac38c9920520e7b5cd87a4ea0f7fa2bde6777a2
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-10-16 01:52:02 +1100

    Change SqlCommand to ObjJson

--- CwRetail.Data/Resources/Subscriptions/SampleDatabase-CreateObjects.sql
+++ CwRetail.Data/Resources/Subscriptions/SampleDatabase-CreateObjects.sql
@@ -7,7 +7,7 @@ CREATE TABLE audit.products (
 	Id BIGINT IDENTITY (1, 1) PRIMARY KEY,
 	EventType NVARCHAR (250) NOT NULL CHECK (LEN(EventType) > 0),
 	LoginName NVARCHAR (250) NOT NULL CHECK (LEN(LoginName) > 0),
-	SqlCommand NVARCHAR (MAX) NOT NULL CHECK (LEN(SqlCommand) > 0),
+	ObjJson NVARCHAR (MAX) NOT NULL CHECK (LEN(ObjJson) > 0),
 	AuditDateTime DATETIME NOT NULL CHECK(CONVERT(DATE, AuditDateTime) = CONVERT(DATE, getdate()))
 )
 GO
@@ -22,7 +22,7 @@ BEGIN
 	SELECT @EventData = EVENTDATA()
 
 	INSERT INTO CwRetail.audit.products 
-	(EventType, LoginName, SqlCommand, AuditDateTime)
+	(EventType, LoginName, ObjJson, AuditDateTime)
 	VALUES
 	(
 	@EventData.value('(/EVENT_INSTANCE/EventType)[1]', 'NVARCHAR (250)'),
@@ -33,12 +33,36 @@ BEGIN
 END
 GO
 
-CREATE TRIGGER production.products_tr_test
+CREATE TRIGGER production.products_tr_delete
 ON CwRetail.production.products
 AFTER DELETE
 AS
 BEGIN
 	SET NOCOUNT ON
-	SELECT Id, Name, Price, Type, Active from deleted
+
+	DECLARE @DeleteId BIGINT
+	SELECT @DeleteId = Id from deleted
+
+	DECLARE @DeleteName NVARCHAR(100)
+	SELECT @DeleteName = Name from deleted
+
+	DECLARE @DeletePrice DECIMAL(18,2)
+	SELECT @DeletePrice = Price from deleted
+
+	DECLARE @DeleteType NVARCHAR(50)
+	SELECT @DeleteType = Type from deleted
+
+	DECLARE @DeleteActive BIT
+	SELECT @DeleteActive = Active from deleted
+
+	INSERT INTO CwRetail.audit.products 
+	(EventType, LoginName, ObjJson, AuditDateTime)
+	VALUES
+	(
+	'DELETE',
+	CONVERT(NVARCHAR(250), CURRENT_USER),
+	'{ "Id" : ' + CAST(@DeleteId AS VARCHAR(max)) + ', "Name" : "' + @DeleteName + '", "Price" : ' + CAST(@DeletePrice AS VARCHAR(max)) + ', "Type" : "' + @DeleteType + '", "Active" : ' + CAST(@DeleteActive AS VARCHAR(max)) + ' }',
+	GETDATE()
+	) 
 END
 GO
