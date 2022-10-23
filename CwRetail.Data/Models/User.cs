e606038ecbd6d4422600d36b5c9d3e0d373de119
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-10-23 23:14:20 +1100

    Introduce ability to create new user

---
+++ CwRetail.Data/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CwRetail.Data.Models
+{
+    public class User
+    {
+        public long UserId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public DateTime LastActive { get; set; }
+    }
+}
---
+++ CwRetail.Data/Repositories/Implementation/UserRepository.cs
@@ -0,0 +1,61 @@
+﻿using CwRetail.Data.Models;
+using CwRetail.Data.Repositories.Interface;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CwRetail.Data.Repositories.Implementation
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly SqlConnection _connection;
+
+        public UserRepository()
+        {
+            _connection = new SqlConnection(ConnectionStrings.Test);
+        }
+
+        public int Insert(User user)
+        {
+            try
+            {
+                _connection.Open();
+
+                string sql = $@"INSERT INTO auth.users
+                                    (                    
+	                                    Username,
+	                                    Email,
+	                                    Phone,
+	                                    LastActive
+                                    )
+                                    VALUES 
+                                    ( 
+	                                    @Username,
+	                                    @Email,
+	                                    @Phone,
+	                                    @LastActive
+                                    )";
+                var result = _connection.Execute(sql, new
+                {
+                    Username = user.Username,
+                    Email = user.Email,
+                    Phone = user.Phone,
+                    LastActive = user.LastActive,
+                });
+
+                _connection.Close();
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+    }
+}
