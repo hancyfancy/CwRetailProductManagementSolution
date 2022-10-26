e7d4249e9956de3fbc055c7fdc912721ebcd4c1d
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-10-26 19:40:52 +1100

    Create InsertOrUpdate repository method for user tokens

---
+++ CwRetail.Data/Repositories/Implementation/UserTokensRepository.cs
@@ -0,0 +1,64 @@
+﻿using CwRetail.Data.Models;
+using CwRetail.Data.Repositories.Interface;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CwRetail.Data.Repositories.Implementation
+{
+    public class UserTokensRepository : IUserTokensRepository
+    {
+        private readonly SqlConnection _connection;
+
+        public UserTokensRepository()
+        {
+            _connection = new SqlConnection(ConnectionStrings.Test);
+        }
+
+        public int InsertOrUpdate(long userId, string token)
+        {
+            try
+            {
+                _connection.Open();
+
+                string sql = $@"IF EXISTS (SELECT UserTokenId FROM auth.usertokens WHERE UserId = @UserId)
+                                BEGIN
+	                                IF (SELECT RefreshAt FROM auth.usertokens WHERE UserId = @UserId) > GETUTCDATE()
+	                                BEGIN
+		                                UPDATE 
+			                                auth.usertokens
+		                                SET	
+			                                Token = @Token,
+			                                RefreshAt = @RefreshAt
+		                                WHERE
+			                                UserId = @UserId
+	                                END
+                                END
+                                ELSE
+                                BEGIN
+                                    INSERT INTO auth.usertokens (UserId, Token, RefreshAt)
+	                                VALUES (@UserId, @Token, @RefreshAt)
+                                END";
+                var result = _connection.Execute(sql, new
+                {
+                    UserId = userId,
+                    Token = token,
+                    RefreshAt = DateTime.UtcNow.AddDays(1)
+                });
+
+                _connection.Close();
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+    }
+}
