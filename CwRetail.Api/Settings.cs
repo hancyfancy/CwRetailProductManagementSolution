c01686c328e2ecc51a75a9d0b6354a2b2f0e420d
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-10-31 17:06:30 +1100

    Update Settings.cs

--- CwRetail.Api/Settings.cs
+++ CwRetail.Api/Settings.cs
@@ -3,7 +3,7 @@
     public static class Settings
     {
         public static readonly string SmtpHost = "smtp.zoho.com.au";
-        public static readonly int SmtpPort = "465";
+        public static readonly int SmtpPort = 465;
         public static readonly bool SmtpUseSsl = true;
         public static readonly string SmtpSender = "malavdea@zohomail.com.au";
         public static readonly string SmtpPassword = "wSxzMhNxJ1NU";
